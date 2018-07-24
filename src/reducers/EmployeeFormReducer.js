6d420b1b48e94eaa963cdc6002c2ed639b1d7c22
chalat chansima <chalat.csm@gmail.com>
2018-07-24 21:39:54 +0700

    Refactor Employee Form to Component and add Modal to popup when press fire button

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,6 +1,7 @@
 import {
  EMPLOYEE_UPDATE,
- EMPLOYEE_CREATE
+ EMPLOYEE_CREATE,
+ EMPLOYEE_SAVE_SUCCESS
 } from '../actions/types';
 
 const INITAIL_STATE = {
@@ -14,6 +15,8 @@ export default (state = INITAIL_STATE, action) => {
     case EMPLOYEE_UPDATE:
       return { ...state, [action.payload.prop]: action.payload.value };
     case EMPLOYEE_CREATE:
+      return INITAIL_STATE;
+    case EMPLOYEE_SAVE_SUCCESS:
       return INITAIL_STATE;
     default:
       return state;
